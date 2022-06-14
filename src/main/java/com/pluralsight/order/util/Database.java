fc2979d1faca00cd634bb16d6d7048b7d4f7f35b
SAURABH7908 <saurabhdhumal95@gmail.com>
2022-06-14 14:49:31 +0530

    module 2

--- src/main/java/com/pluralsight/order/util/Database.java
+++ src/main/java/com/pluralsight/order/util/Database.java
@@ -5,7 +5,9 @@ import org.h2.tools.RunScript;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 /**
  * Singleton class to get database connections
@@ -13,11 +15,22 @@ import java.sql.SQLException;
 public class Database {
     private static Database instance = null;
     private static boolean isInitialized = false;
+    private String url = "jdbc:h2:mem:orders;DB_CLOSE_DELAY=-1";
+    private String user="sa";
+    private String password="";
 
     /**
      * Private constructor
      */
-    private Database() { }
+    private Database() {
+
+    }
+
+    public Database(String url,String user,String password) {
+        this.url = url;
+        this.password = password;
+        this.user = user;
+    }
 
     /**
      * Method that creates an instance of this class (if it's not created already)
@@ -49,7 +62,7 @@ public class Database {
      * @throws SQLException In case of a database error
      */
     public Connection getConnection() throws SQLException {
-        Connection connection = null;
+        Connection connection = DriverManager.getConnection(url,user,password);
 
         if(!isInitialized && connection != null) {
             initializeDatabase(connection);
